e337e9e8f59ddfe247d93ebafc997b2d883e8a06
Camila Cristina <criscamila09@gmail.com>
2022-07-24 18:53:41 -0300

    ifx logo

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,7 +1,9 @@
+import PoliLogo from '../../assets/logo.png'
+
 export function Logo() {
   return (
     <div className="flex align-center w-[70px] border-r  border-slate-500 cursor-pointer">
-      <img src="./src/assets/logo.png" alt="logo_poli" className="p-2" />
+      <img src={PoliLogo} alt="logo_poli" className="p-2" />
     </div>
   )
 }

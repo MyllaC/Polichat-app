abbf253e5785d791e0055fa134b074218fff5edc
Camila Cristina <criscamila09@gmail.com>
2022-07-25 12:16:24 -0300

    margir on mobile

--- src/pages/Mobile.tsx
+++ src/pages/Mobile.tsx
@@ -5,13 +5,13 @@ import { ChatPerContact } from '../components/Chat/ChatPerContact'
 export function Mobile() {
   const { chave } = useParams<{ chave: string }>()
   return (
-    <div className="flex max-h-screen">
+    <div className="flex  max-h-full">
       {!chave ? (
-        <div className="flex flex-1 max-h-screen">
+        <div className="flex flex-1 max-h-full m-2">
           <Contacts />
         </div>
       ) : (
-        <div className="m-4">
+        <div className="m-2">
           <ChatPerContact />
         </div>
       )}
